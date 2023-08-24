b5a8e9c56219b9daf9fa9789f88fc0f47210ee8c
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2023-08-24 09:49:13 +0530

    validation of data entered by user done in auth.js

--- beckend/index.js
+++ beckend/index.js
@@ -3,6 +3,7 @@ const express=require("express");
 var bodyParser = require('body-parser');
 const app=express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //connecting to mongoose
 connectToMongoose();
 //handling requests
--- beckend/routes/auth.js
+++ beckend/routes/auth.js
@@ -1,13 +1,31 @@
-const express=require("express");
-const router=express.Router();
-const User=require("../models_mongoose/User");
-router.post('/',function(req,res){
-    
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+const express = require("express");
+const router = express.Router();
+const User = require("../models_mongoose/User");
+//expess validation(for validation check(data entered by user is correct or not))
+const { body, validationResult } = require('express-validator');
 
-});
+router.post('/',
+    body('name', 'Enter a Valid name').isLength({ min: 5 }),
+    body('Email', 'Enter a valid Email').isEmail(),
+    body('password', 'Entre a Valid password').isLength({ min: 8 }),
+    function (req, res) {
+        //this will give wheather validation is successfull or not and store that in result
+        console.log(req.body);
+        const result = validationResult(req);
+        console.log(result);
+        if (result.isEmpty()) {
+            const user = User(req.body);
+            user.save().then(function (r, s) {
+                console.log("saved successfully");
+                s.send("data saved successfullly");
+            }).catch(function (error) {
+                console.log("Invalid entry");
+                res.status(400).send(error);
+            })
+        }
+        else {
+            res.status(400).send({ errors: result.array() });
+        }
+    });
 
-module.exports=router;
+module.exports = router;
