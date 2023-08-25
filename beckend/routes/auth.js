5d97e25071b9a737385134e56b2d8c18f9df4f35
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2023-08-25 18:36:16 +0530

    salting and hashing of password done using bcrypt

--- beckend/routes/auth.js
+++ beckend/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models_mongoose/User");
 //expess validation(for validation check(data entered by user is correct or not))
 const { body, validationResult } = require('express-validator');
+var bcrypt = require('bcryptjs');
 
 router.post('/',
     body('name', 'Enter a Valid name').isLength({ min: 5 }),
@@ -13,15 +14,27 @@ router.post('/',
         console.log(req.body);
         const result = validationResult(req);
         console.log(result);
+        //hashing a password
+        const pass = req.body.password;
         if (result.isEmpty()) {
-            const user = User(req.body);
-            user.save().then(function (r, s) {
-                console.log("saved successfully");
-                s.send("data saved successfullly");
-            }).catch(function (error) {
-                console.log("Invalid entry");
-                res.status(400).send(error);
-            })
+            bcrypt.genSalt(10, function (err, salt) {
+                bcrypt.hash(pass, salt, function (err, hash) {
+                    // Store hash in your password DB.
+                    const user = new User({
+                        name: req.body.name,
+                        Email: req.body.Email,
+                        password: hash
+                    });
+                    user.save().then(function (r, s) {
+                        console.log("saved successfully");
+                        res.status(200).send("data saved successfully")
+                    }).catch(function (error) {
+                        console.log("Invalid entry");
+                        res.status(400).send(error);
+                    })
+                });
+            });
+
         }
         else {
             res.status(400).send({ errors: result.array() });
